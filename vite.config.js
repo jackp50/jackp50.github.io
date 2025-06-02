89214a887256ebbf00b363e736fc819f456f01d9
jackp50 <jpearne9@gmail.com>
2025-06-01 20:31:07 -0600

    more scipts

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/jackp50.github.io",
+  base: "/jackp50.github.io/",
 })
